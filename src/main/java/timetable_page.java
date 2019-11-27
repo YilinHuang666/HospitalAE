06b8c21061df777d1df36a4fda8df742103f6e3e
yilinhuang666 <alanBetter0117!>
2019-11-28 00:57:02 +0800

    7th commit, client connection test

--- src/main/java/timetable_page.java
+++ src/main/java/timetable_page.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = "/timetable", loadOnStartup = 1)
 
@@ -14,11 +16,14 @@ public class timetable_page extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 
     }
-
+    private static final Logger log = Logger.getLogger(timetable_page.class.getName());
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         response.setContentType("text/html");
         PrintWriter out=response.getWriter();
         out.println("<h2>Timetable</h2>");
+        String reqBody=request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        out.println("<h2>" +reqBody+
+                "</h2>");
     }
 }
