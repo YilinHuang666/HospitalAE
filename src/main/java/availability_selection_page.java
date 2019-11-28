6315421cbfd8e5e4df7c8d807a30d2a425d8c2ff
yilinhuang666 <alanBetter0117!>
2019-11-29 01:25:19 +0800

    8th commit, add time availability selection checkboxes

--- src/main/java/availability_selection_page.java
+++ src/main/java/availability_selection_page.java
@@ -205,6 +205,7 @@ public class availability_selection_page extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println(ta_page);
+        time_slot=request.getParameterValues("time_slot");
         for (String times: time_slot){
            out.println("<h2>"+times+"</h2>");
         }
@@ -212,8 +213,7 @@ public class availability_selection_page extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        time_slot=request.getParameterValues("time_slot");
-        response.sendRedirect("availability_selection_page");
+        //response.sendRedirect("availability_selection_page");
     }
 
 }
