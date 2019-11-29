0b0d91e24703e916d1b43813ec84821df36dc71f
YilinHuang666 <yilin.huang17@imperial.ac.uk>
2019-11-29 13:33:01 +0000

    8th commit, add time availability selection checkboxes

--- src/main/java/welcome_page.java
+++ src/main/java/welcome_page.java
@@ -12,7 +12,6 @@ import java.sql.*;
 public class welcome_page extends HttpServlet {
     private static String firstname;
     private static String lastname;
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
@@ -49,8 +48,7 @@ public class welcome_page extends HttpServlet {
                 "    </style>\n" +
                 "</head>\n" +
                 "<body >\n" +
-                "    <h1>Welcome " +lastname+" "+firstname+
-                        "</h1>\n" +
+                "    <h1>Welcome Yilin Huang</h1>\n" +
                 "    &nbsp;\n" +
                 "    <form action=\"timetable_page\" method=\"post\">\n" +
                 "        <div style=\"text-align: center\" >\n" +
@@ -72,18 +70,22 @@ public class welcome_page extends HttpServlet {
                 "    &nbsp;\n" +
                 "    <form action=\"availability_selection_page\" method=\"post\">\n" +
                 "        <div style=\"text-align: center\">\n" +
-                "            <button class=\"button\" type=\"submit\">Time Availability Selection</button>\n" +
+                "            <button class=\"button\" type=\"button\" name=\"available_page_click\" value=\"avail\">Time Availability Selection</button>\n" +
                 "        </div>\n" +
                 "    </form>\n" +
                 "</body>\n" +
                 "</html>");
-        request.setAttribute("login_doc_firstname",firstname);
-        request.setAttribute("login_doc_lastname",lastname);
+        String button_check = request.getParameter("available_page_click");
+        if (button_check.equals("avail")){
+            request.setAttribute("login_doc_firstname",firstname);
+            request.setAttribute("login_doc_lastname",lastname);
+            RequestDispatcher rd = request.getRequestDispatcher("availability_selection_page");
+            rd.forward(request,response);
+        }
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         String dbUrl =  System.getenv("JDBC_DATABASE_URL");
-
         try {
             Class.forName("org.postgresql.Driver");
         } catch (Exception e) {}
