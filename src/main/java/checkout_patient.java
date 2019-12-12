de163efb8200f3be4b3d49b438271fde49f18cc9
yilinhuang666 <alanBetter0117!>
2019-12-12 08:37:01 +0800

    13th commit, add checkout patient function

--- src/main/java/checkout_patient.java
+++ src/main/java/checkout_patient.java
@@ -22,20 +22,103 @@ public class checkout_patient extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
+                "    <style>\n" +
+                "        body {font-family: Arial, Helvetica, sans-serif;}\n" +
+                "        form {border: 4px solid #f1f1f1;}\n" +
+                "\n" +
+                "        input[type=text], input[type=password] {\n" +
+                "            width: 100%;\n" +
+                "            padding: 15px 25px;\n" +
+                "            margin: 8px 0;\n" +
+                "            display: inline-block;\n" +
+                "            border: 3px solid #ccc;\n" +
+                "            box-sizing: border-box;\n" +
+                "        }\n" +
+                "\n" +
+                "        button {\n" +
+                "            background-color: cornflowerblue;\n" +
+                "            color: white;\n" +
+                "            padding: 14px 10px;\n" +
+                "            margin: 8px 0;\n" +
+                "            border: none;\n" +
+                "            cursor: pointer;\n" +
+                "            width: 100%;\n" +
+                "        }\n" +
+                "\n" +
+                "        button:hover {\n" +
+                "            opacity: 0.8;\n" +
+                "        }\n" +
+                "\n" +
+                "        <!--.cancelbtn {\n" +
+                "            width: auto;\n" +
+                "            padding: 10px 18px;\n" +
+                "            background-color: #f44336;\n" +
+                "        }-->\n" +
+                "\n" +
+                "        .imgcontainer {\n" +
+                "            text-align: center;\n" +
+                "            margin: 24px 0 12px 0;\n" +
+                "        }\n" +
+                "\n" +
+                "        img.avatar {\n" +
+                "            width: 40%;\n" +
+                "            border-radius: 50%;\n" +
+                "        }\n" +
+                "\n" +
+                "        .container {\n" +
+                "            padding: 16px;\n" +
+                "        }\n" +
+                "\n" +
+                "        span.psw {\n" +
+                "            float: right;\n" +
+                "            padding-top: 16px;\n" +
+                "        }\n" +
+                "\n" +
+                "        /* Change styles for span and cancel button on extra small screens */\n" +
+                "        @media screen and (max-width: 300px) {\n" +
+                "            span.psw {\n" +
+                "                display: block;\n" +
+                "                float: none;\n" +
+                "            }\n" +
+                "            .cancelbtn {\n" +
+                "                width: 100%;\n" +
+                "            }\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
                 "<body>\n" +
-                "<h2>Check out Patient</h2>\n" +
-                "<form action=\"checkout_patient\" method='post'>\n" +
-                "    Patient's first name:<br>\n" +
-                "    <input type=\"text\" name=\"firstname\" placeholder=\"Enter patient's firstname\" required>\n" +
-                "    <br><br>\n" +
-                "    Patient's last name:<br>\n" +
-                "    <input type=\"text\" name=\"lastname\" placeholder=\"Enter patient's lastname\" required>\n" +
-                "    <br><br>\n" +
-                "    <input type=\"submit\" value=\"Check out\">\n" +
+                "\n" +
+                "<h2>Checkout Patient</h2>\n" +
+                "\n" +
+                "<form action=\"checkout_patient\" method=\"post\">\n" +
+                "\n" +
+                "    <div class=\"container\">\n" +
+                "        <b>Patient's Firstname</b>\n" +
+                "        <input type=\"text\" placeholder=\"Enter patient's first name to checkout\" name=\"firstname\" required>\n" +
+                "\n" +
+                "        <b>Patient's Lastname</b>\n" +
+                "        <input type=\"password\" placeholder=\"Enter patient's last name to checkout\" name=\"lastname\" required>\n" +
+                "\n" +
+                "        <button type=\"submit\"><h2>Checkout</h2></button>\n" +
+                "       <!-- <label>\n" +
+                "            <input type=\"checkbox\" checked=\"checked\" name=\"remember\"> Remember me\n" +
+                "        </label>-->\n" +
+                "    </div>\n" +
+                "\n" +
+                "   <!-- <div class=\"container\" style=\"background-color:#f1f1f1\">\n" +
+                "        <button type=\"button\" class=\"cancelbtn\">Cancel</button>\n" +
+                "        <span class=\"psw\">Forgot <a href=\"#\">password?</a></span>\n" +
+                "    </div>-->\n" +
                 "</form>\n" +
+                "<form action='welcome_page' method='post'>" +
+                "   <button type='submit'><h2>Return to Welcome Page</h2></button>" +
+                "</form>"+
+                "\n" +
                 "</body>\n" +
-                "</html>");
+                "</html>\n");
     }
 
     @Override
