c7b890496322ddf31f142ceb8dfcb0af9ddbe876
yilinhuang666 <alanBetter0117!>
2019-12-24 23:50:47 +0800

    15th commit, fix little bug

--- src/main/java/checkout_patient.java
+++ src/main/java/checkout_patient.java
@@ -142,7 +142,7 @@ public class checkout_patient extends HttpServlet {
         } catch (SQLException e){
             e.printStackTrace();
         }
-        if (checkout_p_fn!=null && checkout_p_ln!=null) {
+        //if (checkout_p_fn!=null && checkout_p_ln!=null) {
             for (int i = 0; i < c_r_p_fn.size(); i++) {
                 if (checkout_p_fn.equals(c_r_p_fn.get(i)) && checkout_p_ln.equals(c_r_p_ln.get(i))) {
                     patient_to_doctor pd = new patient_to_doctor(checkout_p_fn, checkout_p_ln, d_firstname, d_lastname);
@@ -159,7 +159,7 @@ public class checkout_patient extends HttpServlet {
                     }
                 }
             }
-        }
+        //}
     }
 
     @Override
